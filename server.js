5c39625bf17305fed4369cd61f0b8103b2d5c24c
doadet <davidpom@gmail.com>
2019-08-06 13:44:05 -0400

    messy but functional

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 
 var friends = require("./app/data/friends.js");
 
@@ -23,25 +24,53 @@ app.get("/api/friends", function(req, res) {
     res.json(friends);
 });
 
+function compare(input1, input2) {
+  var totalDifference = 0;
+  for(var i = 0; i < input1.length; i++){
+      totalDifference += Math.abs(input1[i] - input2[i]);
+  }
+  return totalDifference;
+}
+
+
+
 
 app.post("/api/friends", function(req, res) {
-    var newtable = req.body;
+    var newFriendAdd = req.body;
 
-    // newtable.routeName = newtable.customerName.replace(/\s+/g, "").toLowerCase();
-    console.log(newtable);
+    // console.log(friends);
+    var bestMatch;
 
+    for(var i = 0; i < friends.length; i++){
+      var score1 = newFriendAdd.scores;
+      var score2 = friends[i].scores;
+      var numberify1 = [];
+      var numberify2 = [];
+      for(var j = 0; j < score1.length; j++){
+        numberify1.push(Number(score1[j]));
+        numberify2.push(Number(score2[j]));
+      };
+      // console.log("numberify: " + numberify1 + "\n" + numberify2);
+      var score = compare(numberify1, numberify2);
 
-    // if(tables.length<5) {
-    //     tables.push(newtable);
-    //     console.log(tables);
-    //     res.json(newtable);
+      console.log("Score for " + friends[i].name + ": " + score);
+      bestMatch = friends[i];
+      console.log("Best Match: " + bestMatch.name);
+    }
 
-    // } else {
-    //     waitlist.push(newtable);
-    //     console.log("THIS IS ON THE WAITLIST" + waitlist);
-    //     res.json(newtable);
-    // }
+    friends.push(newFriendAdd);
 
+    var createFile = "var friends = " + JSON.stringify(friends) + "; \n module.exports = friends;"
+    fs.writeFile("./app/data/friends.js", createFile, function(err) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log("Content Added!");
+      }
+    });
+
+    res.json(bestMatch);
 
 });
 
