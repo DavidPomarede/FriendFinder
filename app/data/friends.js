b41d9f46556aec81c591efb29608a5be9cd8638e
doadet <davidpom@gmail.com>
2019-08-06 10:23:06 -0400

    server file stuff

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-{
+var friends = [{
   "name":"Ahmed",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
@@ -13,5 +13,24 @@
       4,
       1
     ]
-}
+},
+{
+  "name":"Ben",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      4,
+      1,
+      3,
+      4,
+      4,
+      1
+    ]
+},
+];
 
+
+module.exports = friends;
